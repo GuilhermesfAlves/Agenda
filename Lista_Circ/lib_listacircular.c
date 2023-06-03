b394b6d1c02e236ea10048d6b0161a5b1a2e28ca
Guilherme <guilhermesfalves17@gmail.com>
2023-06-03 12:25:53 -0300

    lista circular

--- Lista_Circ/lib_listacircular.c
+++ Lista_Circ/lib_listacircular.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "lib_listacircular.h"
 
 /* Cria uma Lista vazia */
@@ -32,13 +33,54 @@ void lista_destroi (lista_t **l){
 /* Adiciona um elemento em ordem de acordo com o valor elemento->chave na Lista. 
  * Retorna 1 em caso de sucesso e 0 caso contrario. */
 int lista_insere_ordenado (lista_t *l, elemento_t *elemento){
+    nodo_t *aux, *novo;
 
+    if (!(novo = malloc(sizeof(nodo_t))))
+        return 0;
+
+    aux = l -> ini;
+    novo -> elemento = elemento;
+    if (lista_vazia(l)){
+        l -> ini = novo;
+        novo -> prox = l -> ini;
+        novo -> prev = l -> ini;
+    }
+    else {
+        while ((aux -> prox -> elemento -> chave < elemento -> chave) && (aux -> prox != l -> ini))
+            aux = aux -> prox;
+        if (l -> ini -> elemento -> chave > elemento -> chave){
+            aux = aux -> prev;
+            l -> ini = novo;
+        }
+        novo -> prox = aux -> prox;
+        novo -> prev = aux;
+        aux -> prox = novo;
+        novo -> prox -> prev = novo;
+    }
+    return 1;
 }
 
 /* Retira o elemento da Lista e a mantem em ordem.
  * Retorna 1 em caso de sucesso e 0 caso elemento nao esteja na Lista. */
 int lista_remove_ordenado (lista_t *l, elemento_t *elemento){
+    nodo_t *aux;
 
+    aux = l -> ini;
+    if (!lista_vazia(l)){
+        while ((aux -> prox != l -> ini) && (aux -> elemento -> chave != elemento -> chave))
+            aux = aux -> prox;
+        if (aux == l -> ini)
+            l -> ini = l -> ini -> prox;
+        if (l -> ini -> prox == l -> ini)
+            l -> ini = NULL; /*tira o ultimo elemento*/
+        else { 
+            aux -> prox -> prev = aux -> prev;
+            aux -> prev -> prox = aux -> prox;
+        }
+        free(aux);
+        return 1;
+    }
+    return 0;
 }
 
 /* Retorna 1 se a lista esta vazia e 0 caso contrario */
---
+++ Lista_Circ/testa_lista.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib_listacircular.h"
+#define max 9
+
+lista_t *testa_cria(){
+    lista_t *l;
+    l = lista_cria();
+    if (!l){
+        printf("erro criacao\n");
+        return NULL;
+    }
+    if (!l -> ini)
+        printf("cria certo\n");
+    return l;
+}
+
+void printa_ini_fim(lista_t *lista){
+    nodo_t *aux;
+    
+    aux = lista -> ini;
+    if (!aux)
+        printf("sem cabeca\n ");
+    else
+        while (aux != lista -> ini){
+            printf("%d - ", aux -> elemento -> chave);
+            aux = aux -> prox;
+        }
+}
+
+int testa_insere(lista_t *lista, elemento_t vet[]){
+    int i=0;
+
+    while (i < max){
+        printf("inserindo %d\n", vet[i].chave);
+        if (!lista_insere_ordenado(lista, &vet[i])){
+            printf("erro insere\n");
+            return 0;
+        };
+        printa_ini_fim(lista);
+        i++;
+    };
+    return 1;
+}
+
+void set_element(elemento_t v[]){
+    int i=0;
+
+    while (i < max){
+        v[i].chave = rand() % max;
+        printf ("setado %d: %d\n", i, v[i].chave);
+        i++;
+    }
+}
+
+void tamanho_lista(lista_t *l){
+    nodo_t *aux;
+    int cont=0;
+    
+    aux = l -> ini;
+    if (aux)
+        do {
+            aux = aux -> prox;
+            cont++;
+        } while (aux != l -> ini);
+    printf("lista com %d elementos\n", cont);
+}
+
+void imprim_lista(lista_t *l){
+    nodo_t *aux;
+
+    aux = l -> ini;
+    printf("inicio: %d\n", aux -> elemento -> chave);
+    printf("imprimindo lista:\n");
+    do {
+        printf("item: %d\t", aux -> elemento -> chave);
+        aux = aux -> prox;
+    } while (aux != l -> ini -> prox);
+    printf("tudo impresso\n");
+}
+
+void imprim_infinit(lista_t *lista){
+    nodo_t *aux;
+    int i=0;
+
+    aux = lista -> ini;
+    if (!lista_vazia(lista))
+        do {
+            printf("%d - ", aux -> elemento -> chave);
+            aux = aux -> prox;
+            i++;
+        } while(aux != lista -> ini);
+    printf("\n");
+}
+
+int testa_remove(lista_t *lista, elemento_t vet[]){
+    int i=0;
+
+    /*if (!lista_remove_ordenado(lista, &elem))
+        printf("tudo certo, tentou remover o que nao existia\n");
+    */
+    while(i < max){
+        printf("removendo %d\n", vet[i].chave);
+        imprim_infinit(lista);
+        if (!lista_remove_ordenado(lista, &vet[i])){
+            printf("erro remove\n");
+            return 0;
+        };
+        i++;
+    };
+    return 1;
+}
+
+void imprim_prox_prev(lista_t *lista){
+    nodo_t *aux;
+    int i=0;
+
+    aux = lista -> ini;
+    while(i < max){
+        printf("%d <- %d -> %d \n", aux -> prev -> elemento -> chave, aux -> elemento -> chave, aux -> prox -> elemento -> chave);
+        aux = aux -> prox;
+        i++;
+    }
+}
+
+
+int main(){
+    lista_t *lista;
+    elemento_t v[max];
+
+    set_element(v);
+    if (!(lista = lista_cria())){
+        printf("problema testa_cria\n");
+        return 0;
+    }
+
+    if (!testa_insere(lista,v)){
+        printf("problema testa_insere\n");
+        return 0;
+    }
+    tamanho_lista(lista);
+    /*imprim_lista(lista);*/
+    imprim_infinit(lista);
+    imprim_prox_prev(lista);
+    if (!testa_remove(lista,v)){
+        printf("problema testa_remove\n");
+        return 0;
+    }
+
+    printf ("tudo certo\n");
+    lista_destroi(&lista);
+    return 1;
+}
